a1a868a7283747ea2359ec9c9393d7109db7d4bd
jineq <e115765@iu.u-ryukyu.ac.jp>
2013-11-12 16:26:53 +0900

    use case + scene directer
    画面遷移図を追加しました

---
+++ character status.mm
@@ -0,0 +1,9 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1384043923096" ID="ID_238002887" MODIFIED="1384043936041" TEXT="character status">
+<node CREATED="1384043936433" ID="ID_380016692" MODIFIED="1384043938941" POSITION="right" TEXT="HP"/>
+<node CREATED="1384043939321" ID="ID_1714307670" MODIFIED="1384043941973" POSITION="right" TEXT="ATK"/>
+<node CREATED="1384043942256" ID="ID_170841607" MODIFIED="1384043946526" POSITION="right" TEXT="ATKSpeed"/>
+<node CREATED="1384043946944" ID="ID_1248139513" MODIFIED="1384043952551" POSITION="right" TEXT="moveSpeed"/>
+</node>
+</map>
